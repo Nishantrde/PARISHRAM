31f9258a2fb1cf41ad873fb0c851f267b41a61cf
Nishant <nishant2024rde@gmail.com>
2025-09-14 23:28:51 +0530

    Initial Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def bookings(request):
+    return render(request, "booking.html")
+
+
