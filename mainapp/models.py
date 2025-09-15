41d23b5f0c6ed116b4df05ca4252e30ce9b1d4f5
Nishant <nishant2024rde@gmail.com>
2025-09-15 08:10:51 +0530

    Initial Commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Workers(models.Model):
+    name = models.CharField(max_length=50, default="")
+    occupation = models.CharField(max_length=50, default="")
+    charge_per_hr = models.IntegerField(default=0)
+    ratings = models.FloatField(null=True, blank=True)
+    location = models.CharField(max_length=1000, default="")
+
+
